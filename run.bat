e9abb797738b3788417473f23e59f5ae8949333c
IlyaKrn <ilyakornoukhov@gmail.com>
2024-09-25 18:04:57 +0300

    run script modify

--- run.bat
+++ run.bat
@@ -1,17 +1,45 @@
+@echo off
+
 docker compose down
 echo y|docker system prune -a --volumes
+
+echo ==============================
+echo         DOCKER CLEANED
+echo ==============================
 
 cd identity-service/
 call gradlew clean
 call gradlew bootJar
 
+echo ==============================
+echo   identity-service COMPILED
+echo ==============================
+
 cd database-service/
 call gradlew clean
 call gradlew bootJar
 
+echo ==============================
+echo   database-service COMPILED
+echo ==============================
+
 @rem TODO: clean and rebuild plain-service executing file
 
+@rem echo ==============================
+@rem echo     plain-service COMPILED
+@rem echo ==============================
+
 @rem TODO: clean and rebuild gateway executing file
+
+@rem echo ==============================
+@rem echo        gateway COMPILED
+@rem echo ==============================
+
+
+
+echo ==============================
+echo    STARTING DOCKER COMPOSE
+echo ==============================
 
 cd ../
 docker compose up
