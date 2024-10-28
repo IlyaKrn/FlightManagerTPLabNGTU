5f2bee4b5a470b9ec780b05e32c4c1d95b1e1923
IlyaKrn <ilyakornoukhov@gmail.com>
2024-10-28 20:56:17 +0300

    sample requests + json lib

--- plane-service/src/cpp/controllers/AirportController.cpp
+++ plane-service/src/cpp/controllers/AirportController.cpp
@@ -1,15 +1,48 @@
 #include "../../header/controllers/AirportController.h"
 #include "../../header/Config.h"
 
-void AirportController::configure(httplib::Server* server)
-{
-    server->Get(SAMPLE_MAPPING, [this](const httplib::Request &, httplib::Response &res) {
-        //TODO("replace to here service method invocation and writing result to list")
-        std::string json = "{\"sample\":\"json\"}";
-        //TODO("convert list to json (use additional lib or manually")
+#include <json/single_include/nlohmann/json.hpp>
 
-        res.set_content(json, "application/json");
+using namespace nlohmann;
+using namespace httplib;
+using namespace std;
+
+void AirportController::configure(Server* server)
+{
+    // sample request handlers
+    server->Get(SAMPLE_MAPPING, [this](const Request&, Response& response)
+    {
+        try
+        {
+            //TODO("replace to here service method invocation and writing result to list")
+            json result;
+            result["name"] = "some name";
+            result["id"] = "some id";
+
+            response.set_content(result.dump(), "application/json");
+        }
+        catch (...)
+        {
+            cout << "AirportController::AirportController: exception occured" << endl;
+            response.status = 500;
+        }
     });
 
-    //TODO("write here other api methods with airports")
+    server->Post(SAMPLE_MAPPING, [&](const Request& request, Response& response)
+    {
+        try
+        {
+            //TODO("replace to here service method invocation and writing result to list")
+            json result = json::parse(request.body);
+
+            response.set_content(result.dump(), "application/json");
+        }
+        catch (...)
+        {
+            cout << "AirportController::AirportController: exception occured" << endl;
+            response.status = 500;
+        }
+    });
+
+    //TODO("write here other api handlers with airports")
 }
