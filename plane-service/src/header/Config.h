3e09e37bcea0dd24f3732022ed4a7be3ec2e2bcb
IlyaKrn <ilyakornoukhov@gmail.com>
2024-10-31 19:33:00 +0300

    fix

--- plane-service/src/header/Config.h
+++ plane-service/src/header/Config.h
@@ -2,7 +2,7 @@
 #include <string>
 
 constexpr int SERVER_PORT = 8083;
-const std::string SERVER_HOST = "127.0.0.1";
+const std::string SERVER_HOST = "0.0.0.0";
 const std::string SERVICE_TOKEN_NAME = "Service-Token";
 const std::string SERVICE_TOKEN_VALUE = "123";
 
