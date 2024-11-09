f321139f3de05ad975cdd456ef01a99c7e6d2d88
Дмитрий Смородин <smorodindima12@gmail.com>
2024-11-09 12:08:08 +0300

    Plane

--- plane-service/src/cpp/controllers/PlaneController.cpp
+++ plane-service/src/cpp/controllers/PlaneController.cpp
@@ -10,73 +10,133 @@ using namespace std;
 
 void PlaneController::configure(Server* server)
 {
-    // sample request handlers
-    server->Get(PLANE_GET_MAPPING, [this](const Request&, Response& response)
+    server->Get(PLANE_GET_ALL_MAPPING, [this](const Request& req, Response& res)
     {
         try
         {
-
-
-            json result;
-            response.set_content(result.dump(), "application/json");
-        }
-        catch (...)
+            pmr::list<PlaneModel> planes = serv.getAllPlanes();
+            if (!planes.empty())
+            {
+                json planes_json = json::array();
+                for (auto plane : planes)
+                {
+                    json plane_json;
+                    plane_json["id"] = plane.getId();
+                    plane_json["name"] = plane.getName();
+                    plane_json["pilot"] = plane.getPilot();
+                    plane_json["builtYear"] = plane.getBuiltYear();
+                    plane_json["brokenPercentage"] = plane.getBrokenPercentage();
+                    plane_json["speed"] = plane.getSpeed();
+                    plane_json["minAirportSize"] = plane.getMinAirportSize();
+                    planes_json.push_back(plane_json);
+                }
+                res.status = 200;
+                res.set_content(planes_json.dump(), "application/json");
+            } else
+            {
+                res.status = 404;
+                res.set_content("Аэропорты не найдены", "text/plane");
+            }
+        } catch (const std::exception& e)
         {
-            cout << "PlaneController::PlaneController: exception occured" << endl;
-            response.status = 500;
+            cout << "PlaneController::PlaneController: exception occured" << e.what() << endl;
+            res.status = 500;
         }
     });
 
-    server->Post(PLANE_CREATE_MAPPING, [&](const Request& request, Response& response)
+    server->Post(PLANE_CREATE_MAPPING, [&](const Request& req, Response& res)
     {
         try
         {
-
-            json result = json::parse(request.body);
-
-            response.set_content(result.dump(), "application/json");
-        }
-        catch (...)
+            json plane_json = json::parse(req.body);
+            PlaneModel plane(plane_json["id"], plane_json["name"], plane_json["pilot"], plane_json["builtYear"], plane_json["brokenPercentage"], plane_json["speed"], plane_json["minAirportSize"]);
+            bool created = serv.createPlane(plane);
+            if (created)
+            {
+                res.status = 201;
+                res.set_content(plane_json.dump(), "application/json");
+            } else
+            {
+                res.status = 400;
+                res.set_content("Не удалось создать аэропорт", "text/plane");
+            }
+        } catch (const std::exception& e)
         {
-            cout << "PlaneController::PlaneController: exception occured" << endl;
-            response.status = 500;
+            cout << "PlaneController::PlaneController: exception occured" << e.what() << endl;
+            res.status = 500;
         }
     });
-
-    server->Post(PLANE_UPDATE_MAPPING, [&](const Request& request, Response& response)
+    server->Post(PLANE_UPDATE_MAPPING, [&](const Request& req, Response& res)
     {
         try
         {
-
-        }
-        catch (...)
+            std::string fields = req.get_param_value("update");
+            json plane_json = json::parse(req.body);
+            PlaneModel plane(plane_json["id"], plane_json["name"], plane_json["pilot"], plane_json["builtYear"], plane_json["brokenPercentage"], plane_json["speed"], plane_json["minAirportSize"]);
+            bool updated = serv.updatePlane(plane, fields);
+            if (updated)
+            {
+                res.status = 200;
+                res.set_content("Обновлены поля " + fields + " у самолёта с id " + std::to_string(plane.getId()), "text/plane");
+            } else
+            {
+                res.status = 400;
+                res.set_content("Не удалось обновить самолёт", "text/plane");
+            }
+        } catch (const std::exception& e)
         {
-            cout << "PlaneController::PlaneController: exception occured" << endl;
-            response.status = 500;
+            cout << "PlaneController::PlaneController: exception occured" << e.what() << endl;
+            res.status = 500;
         }
     });
-    server->Get(PLANE_GET_MAPPING, [&](const Request& request, Response& response)
+    server->Delete(PLANE_DELETE_MAPPING + R"(/(\d+))", [&](const Request& req, Response& res)
     {
         try
         {
-
-        }
-        catch (...)
+            int id = stoi(req.matches[1]);
+            bool deleted = serv.deletePlane(id);
+            if (deleted)
+            {
+                res.status = 200;
+            } else
+            {
+                res.status = 404;
+                res.set_content("Самолет с таким id не найден", "text/plane");
+            }
+        } catch (const std::exception& e)
         {
-            cout << "PlaneController::PlaneController: exception occured" << endl;
-            response.status = 500;
+            cout << "PlaneController::PlaneController: exception occured" << e.what() << endl;
+            res.status = 500;
         }
     });
-    server->Delete(PLANE_DELETE_MAPPING, [&](const Request& request, Response& response)
+    server->Get(PLANE_GET_BY_ID_MAPPING, [this](const Request& req, Response& res)
     {
         try
         {
-
-        }
-        catch (...)
+            int id = stoi(req.get_param_value("id"));
+            PlaneModel plane = serv.getPlaneById(id);
+            PlaneModel empty_plane;
+            if (plane.getId() != empty_plane.getId())
+            {
+                json plane_json;
+                plane_json["id"] = plane.getId();
+                plane_json["name"] = plane.getName();
+                plane_json["pilot"] = plane.getPilot();
+                plane_json["builtYear"] = plane.getBuiltYear();
+                plane_json["brokenPercentage"] = plane.getBrokenPercentage();
+                plane_json["speed"] = plane.getSpeed();
+                plane_json["minAirportSize"] = plane.getMinAirportSize();
+                res.status = 200;
+                res.set_content(plane_json.dump(), "application/json");
+            } else
+            {
+                res.status = 404;
+                res.set_content("Не найден самолет с таким id", "text/plane");
+            }
+        } catch (const std::exception& e)
         {
-            cout << "PlaneController::PlaneController: exception occured" << endl;
-            response.status = 500;
+            cout << "PlaneController::PlaneController: exception occured" << e.what() << endl;
+            res.status = 500;
         }
     });
 }
--- plane-service/src/cpp/services/PlaneService.cpp
+++ plane-service/src/cpp/services/PlaneService.cpp
@@ -1,1 +1,76 @@
 #include "../../header/services/PlaneService.h"
+
+std::pmr::list<PlaneModel> PlaneService::getAllPlanes()
+{
+    std::pmr::list<PlaneModel> planes = repo.getPlanes("");
+    if (!planes.empty())
+    {
+        return planes;
+    }
+    return std::pmr::list<PlaneModel>();
+}
+PlaneModel PlaneService::getPlaneById(int id)
+{
+    std::pmr::list<PlaneModel> planes = repo.getPlanes(std::to_string(id));
+    PlaneModel empty_plane;
+    for (auto plane : planes)
+    {
+        if (plane.getId() != empty_plane.getId())
+        {
+            return plane;
+        }
+    }
+    return empty_plane;
+}
+bool PlaneService::createPlane(PlaneModel plane)
+{
+    bool res = repo.createPlane(plane);
+    if (res)
+    {
+        return true;
+    }
+    return false;
+}
+bool PlaneService::deletePlane(int id)
+{
+    bool res = repo.deletePlane(id);
+    if (res)
+    {
+        return true;
+    }
+    return false;
+}
+bool PlaneService::updatePlane(PlaneModel plane, std::string update)
+{
+    PlaneModel n_plane = getPlaneById(plane.getId());
+    if (update.find("name") != std::string::npos)
+    {
+        n_plane.setName(plane.getName());
+    }
+    if (update.find("pilot") != std::string::npos)
+    {
+        n_plane.setPilot(plane.getPilot());
+    }
+    if (update.find("builtYear") != std::string::npos)
+    {
+        n_plane.setBuiltYear(plane.getBuiltYear());
+    }
+    if (update.find("brokenPercentage") != std::string::npos)
+    {
+        n_plane.setBrokenPercentage(plane.getBrokenPercentage());
+    }
+    if (update.find("speed") != std::string::npos)
+    {
+        n_plane.setSpeed(plane.getSpeed());
+    }
+    if (update.find("minAirportSize") != std::string::npos)
+    {
+        n_plane.setMinAirportSize(plane.getMinAirportSize());
+    }
+    bool res = repo.updatePlane(n_plane, update);
+    if (res)
+    {
+        return true;
+    }
+    return false;
+}
