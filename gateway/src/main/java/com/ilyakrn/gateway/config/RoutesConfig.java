ba408f26a7b7e03b3df5714bc0b60a6a4b79c7fb
IlyaKrn <ilyakornoukhov@gmail.com>
2024-11-13 19:35:46 +0300

    reformat code + logs

--- gateway/src/main/java/com/ilyakrn/gateway/config/RoutesConfig.java
+++ gateway/src/main/java/com/ilyakrn/gateway/config/RoutesConfig.java
@@ -2,12 +2,14 @@ package com.ilyakrn.gateway.config;
 
 import com.ilyakrn.gateway.routes.IdentityServiceRoutes;
 import com.ilyakrn.gateway.routes.PlaneServiceRoutes;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class RoutesConfig {
 
@@ -29,6 +31,7 @@ public class RoutesConfig {
                     .path(mapping)
                     .filters(f -> f.addRequestHeader(cfg.getServiceTokenHeader(), cfg.getServiceToken()))
                     .uri(identityServiceRoutes.getUrl()));
+            log.info("added route: {} {}", identityServiceRoutes.getUrl(), mapping);
         }
 
         String[] mappingsPlane = planeServiceRoutes.getMappings();
@@ -37,6 +40,7 @@ public class RoutesConfig {
                     .path(mapping)
                     .filters(f -> f.addRequestHeader(cfg.getServiceTokenHeader(), cfg.getServiceToken()))
                     .uri(planeServiceRoutes.getUrl()));
+            log.info("added route: {} {}", planeServiceRoutes.getUrl(), mapping);
         }
         return b.build();
     }
