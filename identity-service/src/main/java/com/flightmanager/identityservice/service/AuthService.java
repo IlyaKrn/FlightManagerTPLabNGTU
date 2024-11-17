6a7b7e30ef913018dc1066a3f45a2972ce14cc83
IlyaKrn <ilyakornoukhov@gmail.com>
2024-11-18 01:39:28 +0300

    permissions identity

--- identity-service/src/main/java/com/flightmanager/identityservice/service/AuthService.java
+++ identity-service/src/main/java/com/flightmanager/identityservice/service/AuthService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -47,7 +48,62 @@ public class AuthService {
     }
 
     public boolean authorize(String token, Set<String> permissions) {
-        return token != null && jwt.extractId(token) != null && !jwt.isTokenExpired(token) && userRepo.getById(Long.parseLong(jwt.extractId(token))) != null;
+        try {
+            if (permissions == null || permissions.isEmpty())
+                return true;
+            if (token == null || jwt.extractId(token) == null || jwt.isTokenExpired(token))
+                return false;
+            UserModel u = userRepo.getById(Long.parseLong(jwt.extractId(token))).get(0);
+            if (u == null)
+                return false;
+
+            Set<String> permsForDispatcher = new HashSet<>();
+            Set<String> permsForAdmin = new HashSet<>();
+
+            permsForDispatcher.add("airport-get");
+            permsForDispatcher.add("airport-create");
+            permsForDispatcher.add("airport-update");
+            permsForDispatcher.add("airport-delete");
+
+            permsForDispatcher.add("plane-get");
+            permsForDispatcher.add("plane-create");
+            permsForDispatcher.add("plane-update");
+            permsForDispatcher.add("plane-delete");
+
+            permsForDispatcher.add("flight-get");
+            permsForDispatcher.add("flight-create");
+
+            permsForDispatcher.add("time-get");
+            permsForDispatcher.add("time-skip");
+
+            permsForDispatcher.add("dispatcher-get-public " + u.getId());
+            permsForDispatcher.add("dispatcher-get-private " + u.getId());
+
+            permsForDispatcher.add("dispatcher-update-public " + u.getId());
+            permsForDispatcher.add("dispatcher-update-private " + u.getId());
+
+            permsForAdmin.add("dispatcher-get-public");
+            permsForAdmin.add("dispatcher-get-private " + u.getId());
+
+            permsForAdmin.add("dispatcher-update-public");
+            permsForAdmin.add("dispatcher-update-private " + u.getId());
+
+            for (String p : permissions) {
+                if (permsForDispatcher.contains(p) && u.getRoles().contains(RoleModel.DISPATCHER)) {
+                    continue;
+                }
+                if (permsForAdmin.contains(p) && u.getRoles().contains(RoleModel.ADMIN)) {
+                    continue;
+                }
+                if (permsForDispatcher.contains(p) && permsForAdmin.contains(p) && (u.getRoles().contains(RoleModel.DISPATCHER) || u.getRoles().contains(RoleModel.ADMIN))) {
+                    continue;
+                }
+                return false;
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public Long getIdFromToken(String token) {
