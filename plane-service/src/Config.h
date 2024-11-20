447bf705329bf94bf53af601dcc80c63ca3ef268
IlyaKrn <ilyakornoukhov@gmail.com>
2024-11-20 23:57:10 +0300

    remove extra mappings

--- plane-service/src/Config.h
+++ plane-service/src/Config.h
@@ -47,14 +47,12 @@ const std::string GET_ID_BY_TOKEN_MAPPING = "/api/auth/getIdByToken";
 const std::string AUTHORIZE_MAPPING = "/api/auth/authorize";
 
 const std::string AIRPORT_GET_ALL_MAPPING = "/api/airport/getAll";
-const std::string AIRPORT_GET_BY_ID_MAPPING = "/api/airport/get";
 const std::string AIRPORT_CREATE_MAPPING = "/api/airport/create";
 const std::string AIRPORT_UPDATE_MAPPING = "/api/airport/update";
 const std::string AIRPORT_DELETE_MAPPING = "/api/airport/delete";
 
 
 const std::string PLANE_GET_ALL_MAPPING = "/api/plane/getAll";
-const std::string PLANE_GET_BY_ID_MAPPING = "/api/plane/get";
 const std::string PLANE_CREATE_MAPPING = "/api/plane/create";
 const std::string PLANE_UPDATE_MAPPING = "/api/plane/update";
 const std::string PLANE_DELETE_MAPPING = "/api/plane/delete";
@@ -64,7 +62,6 @@ const std::string DISPATCHER_GET_BY_ID_MAPPING = "/api/dispatcher/get";
 const std::string DISPATCHER_UPDATE_MAPPING = "/api/dispatcher/update";
 
 const std::string FLIGHT_GET_ALL_MAPPING = "/api/flights/getAll";
-const std::string FLIGHT_GET_BY_ID_MAPPING = "/api/flights/get";
 const std::string FLIGHT_CREATE_MAPPING = "/api/flights/create";
 
 const std::string TIME_SKIP_MAPPING = "/api/time/skip";
