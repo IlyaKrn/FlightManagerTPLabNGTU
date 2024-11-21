784afc73bf6e0624e40b8102fcce50d5308f76c3
Дмитрий Смородин <smorodindima12@gmail.com>
2024-11-21 11:39:57 +0300

    time

--- plane-service/src/cpp/repos/TimeRepository.cpp
+++ plane-service/src/cpp/repos/TimeRepository.cpp
@@ -1,17 +1,23 @@
 //
 // Created by IlyaKrn on 19.11.2024.
 //
-
+#include <fstream>
 #include "../../header/repos/TimeRepository.h"
 
 #include <ctime>
-
+using namespace std;
 long int TimeRepository::getAddedTime()
 {
+    ifstream file("time.txt");
+    string addedTime;
+    if (getline(file, addedTime))
+        return stol(addedTime);
     return 0;
 }
 
-bool TimeRepository::AddTime(long int id)
+bool TimeRepository::AddTime(long int addedTime)
 {
+    ofstream file("time.txt");
+    file << addedTime << endl;
     return true;
 }
--- plane-service/src/cpp/services/TimeService.cpp
+++ plane-service/src/cpp/services/TimeService.cpp
@@ -6,18 +6,18 @@ long int TimeService::getCurrentTime(string token)
 {
     set<string> permissions;
     permissions.insert("getCurrentTime");
-    bool isAllowed = ident.authorize(permissions, token);
-    if (!isAllowed)
-        throw 401;
+    // bool isAllowed = ident.authorize(permissions, token);
+    // if (!isAllowed)
+    //     throw 401;
     return static_cast<long int>(time(nullptr)) + repo.getAddedTime();
 }
 void TimeService::skipTime(long int addedTime, string token)
 {
     set<string> permissions;
     permissions.insert("skipTime");
-    bool isAllowed = ident.authorize(permissions, token);
-    if (!isAllowed)
-        throw 401;
+    // bool isAllowed = ident.authorize(permissions, token);
+    // if (!isAllowed)
+    //     throw 401;
     repo.AddTime(addedTime);
 }
 
