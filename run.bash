78bcff779d5a39845f5f6a745e5fa5f5f5163253
IlyaKrn <ilyakornoukhov@gmail.com>
2024-11-26 22:53:44 +0300

    rename

--- run.bash
+++ run.bash
@@ -3,27 +3,27 @@ sudo docker compose down
 
 echo "[script] Building database-service ..."
 cd database-service
-sudo chmod +x script.bash
-sudo ./script.bash -bd
+sudo chmod +x service.bash
+sudo ./service.bash -bd
 cd ..
 
 echo "[script] Building identity-service ..."
 cd identity-service
-sudo chmod +x script.bash
-sudo ./script.bash -bd
+sudo chmod +x service.bash
+sudo ./service.bash -bd
 cd ..
 
 echo "[script] Building gateway ..."
 cd gateway
-sudo chmod +x script.bash
-sudo ./script.bash -bd
+sudo chmod +x service.bash
+sudo ./service.bash -bd
 cd ..
 
 echo "[script] Building plane-service ..."
 
 cd plane-service
-sudo chmod +x script.bash
-sudo ./script.bash -bd
+sudo chmod +x service.bash
+sudo ./service.bash -bd
 cd ..
 
 echo "[script] Starting Docker services ..."
