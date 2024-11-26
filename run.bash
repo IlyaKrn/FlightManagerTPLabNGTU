8546bcca4805182ea73e8ba48fdce0ee99920d78
richarlisooon <smorodindima12@gmail.com>
2024-11-26 23:08:23 +0300

    run bash update

--- run.bash
+++ run.bash
@@ -1,30 +1,47 @@
 #!/bin/bash
 sudo docker compose down
+function docker_compose {
+    echo "[script] Building database-service ..."
+    cd database-service
+    sudo chmod +x service.bash
+    sudo ./service.bash -bd
+    cd ..
 
-echo "[script] Building database-service ..."
-cd database-service
-sudo chmod +x service.bash
-sudo ./service.bash -bd
-cd ..
+    echo "[script] Building identity-service ..."
+    cd identity-service
+    sudo chmod +x service.bash
+    sudo ./service.bash -bd
+    cd ..
 
-echo "[script] Building identity-service ..."
-cd identity-service
-sudo chmod +x service.bash
-sudo ./service.bash -bd
-cd ..
+    echo "[script] Building gateway ..."
+    cd gateway
+    sudo chmod +x service.bash
+    sudo ./service.bash -bd
+    cd ..
 
-echo "[script] Building gateway ..."
-cd gateway
-sudo chmod +x service.bash
-sudo ./service.bash -bd
-cd ..
+    echo "[script] Building plane-service ..."
 
-echo "[script] Building plane-service ..."
+    cd plane-service
+    sudo chmod +x service.bash
+    sudo ./service.bash -bd
+    cd ..
 
-cd plane-service
-sudo chmod +x service.bash
-sudo ./service.bash -bd
-cd ..
+    echo "[script] Starting Docker services ..."
+    sudo docker compose up
+}
+function docker_less {
+    echo "not realized yet"
+}
 
-echo "[script] Starting Docker services ..."
-sudo docker compose up
+case "$1" in
+    -d)
+      docker_compose
+      ;;
+    -dl)
+      docker_less
+      ;;
+    *)
+      echo "Usage: $0 {-d|-dl}"
+      exit 1
+      ;;
+esac
