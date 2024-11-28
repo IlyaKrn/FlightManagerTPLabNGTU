7835b6a23870ca5680ac59315954d554e0f9482e
Дмитрий Смородин <smorodindima12@gmail.com>
2024-11-28 12:39:24 +0300

    plane coordinate fixes

--- plane-service/src/cpp/services/PlaneService.cpp
+++ plane-service/src/cpp/services/PlaneService.cpp
@@ -6,7 +6,8 @@
 
 bool PlaneSortByTime(FlightModel a, FlightModel b)
 {
-    return a.getTimestampEnd() > b.getTimestampEnd();
+    return a.getTimestampEnd() >
+    b.getTimestampEnd();
 }
 using namespace std;
 list<PlaneModelResponse> PlaneService::getAllPlanes(string token)
@@ -22,64 +23,74 @@ list<PlaneModelResponse> PlaneService::getAllPlanes(string token)
         //create object planeResponse
         PlaneModelResponse planeResponse(planes.front().getId(), planes.front().getName(), planes.front().getPilot(), planes.front().getBuiltYear(), planes.front().getBrokenPercentage(), planes.front().getSpeed(), planes.front().getMinAirportSize(), 0, 0);
         long int planeId = planes.front().getId();
+        int speed = plane.getSpeed();
         //Taking all flights with our plane
         list<FlightModel> flights = flight.getFlights(nullptr, nullptr, nullptr, nullptr,&planeId);
         if (!flights.empty())
         {
             //If we found flights, we search last flight and, if that's it, current flight
-            long int last_flight_time = 0; //in there we will have last flight time
-            FlightModel last_flight(0,0,0,0,0,0);
-            FlightModel current_fly(0,0,0,0,0,0);
             flights.sort(PlaneSortByTime);
-            if (flights.front().getTimestampEnd() > static_cast<long int>(time(0)) + timer.getAddedTime())
-            {
-                current_fly = flights.front();
-                auto it = next(flights.begin(), 1);
-                last_flight = *it;
-            }
-            else
-                last_flight = flights.front();
-            if (last_flight.getAirportId())
+            bool have_last_flight = true;
+            bool have_current_flight = true;
+            if (flights.front().getTimestampEnd() > static_cast<long int>(time(nullptr)) + timer.getAddedTime()) {
+                if (flights.size() < 2)
+                    have_last_flight = false;
+            } else
+                have_current_flight = false;
+            double x1;
+            double y1;
+            if (have_last_flight) //if our plane have last flight, we are looking for airport from this flight
             {
                 //Found airport from what plane took off
+                auto it = flights.begin();
+                if (have_current_flight)
+                    it++;
+                FlightModel last_flight = *it;
                 long int last_flight_air_id = last_flight.getAirportId();
                 list<AirportModel> airport1 = air.getAirports(&last_flight_air_id);
                 if (airport1.empty())
                     throw 404;
-                double x1 = airport1.front().getX(); //start airport coordinate x
-                double y1 = airport1.front().getY(); //start airport coordinate y
-                int speed = planes.front().getSpeed(); //plane speed
-                if (current_fly.getId() != 0)
-                {
-                    //If we found current flight, we will find airport from that flight
-                    long int current_flight_air_id = current_fly.getAirportId();
-                    list<AirportModel> airport2 = air.getAirports(&current_flight_air_id);
-                    if (airport2.empty())
-                        throw 404;
-                    double x2 = airport2.front().getX(); //end airport coordnate x
-                    double y2 = airport2.front().getY(); //end airport coordinate y
-                    double length = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); //lenght of flight
-                    long int elapsedTime = timer.getAddedTime() + static_cast<long int>(time(0)) - current_fly.getTimestampStart();
-                    //Calculating airport coordinates
-                    double newX = x1 + (x2 - x1) * (speed * elapsedTime / length);
-                    double newY = y1 + (y2 - y1) * (speed * elapsedTime / length);
-                    planeResponse.setX(newX);
-                    planeResponse.setY(newY);
-                } else
-                {
-                    //If we didn't find executing flight, we set coordinates of start airport
-                    planeResponse.setX(x1);
-                    planeResponse.setY(y1);
-                }
+                x1 = airport1.front().getX(); //start airport coordinate x
+                y1 = airport1.front().getY(); //start airport coordinate y
+            } else { //if we didn't find last flight we take first airport from list of airports
+                list<AirportModel> airport1 = air.getAirports();
+                if (airport1.empty())
+                    throw 404;
+                x1 = airport1.front().getX(); //start airport coordinate x
+                y1 = airport1.front().getY(); //start airport coordinate y
             }
+            if (have_current_flight)
+            {
+                FlightModel current_flight = flights.front();
+                //If we found current flight, we will find airport from that flight
+                long int current_flight_air_id = current_flight.getAirportId();
+                list<AirportModel> airport2 = air.getAirports(&current_flight_air_id);
+                if (airport2.empty())
+                    throw 404;
+                double x2 = airport2.front().getX(); //end airport coordnate x
+                double y2 = airport2.front().getY(); //end airport coordinate y
+                double length = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); //length of flight
+                long int elapsedTime = timer.getAddedTime() + static_cast<long int>(time(nullptr)) - current_flight.getTimestampStart();
+                //Calculating airport coordinates
+                double newX = x1 + (x2 - x1) * (speed * elapsedTime / length);
+                double newY = y1 + (y2 - y1) * (speed * elapsedTime / length);
+                planeResponse.setX(newX);
+                planeResponse.setY(newY);
+            } else
+            {
+                //If we didn't find executing flight, we set coordinates of start airport
+                planeResponse.setX(x1);
+                planeResponse.setY(y1);
+            }
+
         } else
         {
-            //If plane didn't fly yer, we take coordinates of airport, that first in list of airports.
-            list<AirportModel> airport = air.getAirports();
-            if (airport.empty())
+            //If plane didn't fly yet, we take coordinates of airport, that first in list of airports.
+            list<AirportModel> airports = air.getAirports();
+            if (airports.empty())
                 throw 404;
-            planeResponse.setX(airport.front().getX());
-            planeResponse.setY(airport.front().getY());
+            planeResponse.setX(airports.front().getX());
+            planeResponse.setY(airports.front().getY());
         }
         planesResponse.push_back(planeResponse);
     }
